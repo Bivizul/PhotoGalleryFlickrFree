08bdfa196cc9b43ee7d12af7f47906eac4cf7432
bivizul <mamochapa@gmail.com>
2022-03-23 18:54:58 +0200

    Project

--- app/src/main/java/com/bivizul/photogalleryflickrfree/NotificationReceiver.kt
+++ app/src/main/java/com/bivizul/photogalleryflickrfree/NotificationReceiver.kt
@@ -1,15 +1,27 @@
 package com.bivizul.photogalleryflickrfree
 
+import android.app.Activity
+import android.app.Notification
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
 
 private const val TAG = "NotificationReceiver"
 
 /** Широковещательный приемник */
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        Log.i(TAG, "received brodcast: ${intent.action}")
+        Log.i(TAG, "received result: $resultCode")
+        if (resultCode != Activity.RESULT_OK) {
+            /** Активность переднего плана отменила возврат трансляции */
+        }
+
+        val requestCode = intent.getIntExtra(PollWorker.REQUEST_CODE, 0)
+        val notification: Notification? = intent.getParcelableExtra(PollWorker.NOTIFICATION)
+        val notificationManager = NotificationManagerCompat.from(context)
+        notification?.let { notificationManager.notify(requestCode, it) }
     }
 }
--- app/src/main/java/com/bivizul/photogalleryflickrfree/PollWorker.kt
+++ app/src/main/java/com/bivizul/photogalleryflickrfree/PollWorker.kt
@@ -1,5 +1,6 @@
 package com.bivizul.photogalleryflickrfree
 
+import android.app.Notification
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
@@ -72,18 +73,33 @@ class PollWorker(val context: Context, workerParams: WorkerParameters) :
                 .setAutoCancel(true)
                 .build()
 
-            val notificationManager = NotificationManagerCompat.from(context)
-            /** Размещения нашего уведомления */
+            /*val notificationManager = NotificationManagerCompat.from(context)
+            */
+            /** Размещения нашего уведомления *//*
             notificationManager.notify(0, notification)
+            */
+            /** Отправка широковещательного интента с разрешением *//*
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)*/
 
-            /** Отправка широковещательного интента с разрешением */
-            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)
+            /** Отправка упорядоченных широковещательных интентов */
+            showBackgroundNotification(0, notification)
         }
         return Result.success()
     }
 
-    companion object{
+    private fun showBackgroundNotification(requestCode: Int, notification: Notification){
+        val intent = Intent(ACTION_SHOW_NOTIFICATION).apply {
+            putExtra(REQUEST_CODE,requestCode)
+            putExtra(NOTIFICATION,notification)
+        }
+        /** Гарантируем, что трансляция будет доставлена приемнику вовремя */
+        context.sendOrderedBroadcast(intent, PERM_PRIVATE)
+    }
+
+    companion object {
         const val ACTION_SHOW_NOTIFICATION = "com.bivizul.photogalleryflickrfree.SHOW_NOTIFICATION"
         const val PERM_PRIVATE = "com.bivizul.photogalleryflickrfree.PRIVATE"
+        const val REQUEST_CODE = "REQUEST_CODE"
+        const val NOTIFICATION = "NOTIFICATION"
     }
 }
